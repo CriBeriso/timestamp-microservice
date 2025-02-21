2eb9dfc5b0855b05a7c331126adf4d7a2da19e0a
CrisBeriso <berith93@icloud.com>
2025-02-21 13:09:20 +0000

    develop change

--- index.js
+++ index.js
@@ -5,10 +5,38 @@
 var express = require('express');
 var app = express();
 
+app.get("/", (req, res) => {
+  res.send("Bienvenido al Timestamp Microservice!");
+});
+
+app.get("/api/:date?", (req, res) => {
+  let { date } = req.params;
+  const regex = /\d{4}-\d{2}-\d{2}/;
+
+  if (date) {
+    if (regex.test(date)) {
+      date = new Date(date);
+    } else {
+      date = parseInt(date);
+      date = new Date(date);
+      if (date == "Invalid Date") {
+        res.json({ error: "Invalid Date" });
+        return;
+      }
+    }
+  } else {
+    date = new Date(); // Si no hay parámetro, usar fecha actual
+  }
+
+  console.log(date.getTime())
+  res.json({ unix: date.getTime(), utc: date.toUTCString() })
+
+});
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,7 +49,7 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
 
